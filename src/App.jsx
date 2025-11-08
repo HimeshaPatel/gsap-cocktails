7da6f6cc07364e458e742028e9adc801fa06a409
himesha <himesha.patel7@gmail.com>
2025-11-08 15:23:32 +1100

    implement the menu section

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Hero from "./components/Hero.jsx";
 import Cocktails from "./components/Cocktails.jsx";
 import About from "./components/About.jsx";
 import Art from "./components/Art.jsx";
+import Menu from "./components/Menu.jsx";
 
 const App = () => {
     return (
@@ -19,6 +20,8 @@ const App = () => {
            <Cocktails />
            <About />
            <Art />
+           <Menu />
+
        </main>
     )
 }
